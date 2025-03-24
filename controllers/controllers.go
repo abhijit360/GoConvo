8727ef536ff41b4ee5d6be4d7a3ff73c3ec45793
abhijit <kamathabhijit2002@gmail.com>
2025-03-23 21:49:27 -0400

    adding a simple websocket listener

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,8 +1,14 @@
 package controllers
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
 )
 
 var Router = http.NewServeMux()
@@ -15,11 +21,33 @@ func goConvoHomePage(w http.ResponseWriter, r *http.Request) {
 
 func getChatHistory(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		fmt.Println("getting the chat with id %v", r.PathValue("id"))
+		fmt.Printf("getting the chat with id %v", r.PathValue("id"))
 	}
+}
+
+func chatWebSocket(w http.ResponseWriter, r *http.Request) {
+	c, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		log.Println("Error Accepting websocket connections", err)
+	}
+	defer c.CloseNow()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	var v interface{}
+	err = wsjson.Read(ctx, c, &v)
+	if err != nil {
+		log.Println("Error Reading from websocket", err)
+	}
+
+	log.Printf("received: %v", v)
+
+	c.Close(websocket.StatusNormalClosure, "")
 }
 
 func CreateControllers() {
 	Router.HandleFunc("/", goConvoHomePage)
 	Router.HandleFunc("/chat/{id}", getChatHistory)
+	Router.HandleFunc("/chat/{id}/", chatWebSocket) // this will be on the route with "ws" prefix
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/abhijit360/GoConvo
 
 go 1.24.1
+
+require github.com/coder/websocket v1.8.13 // indirect
