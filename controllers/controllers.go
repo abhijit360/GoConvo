2cbc3e9aa06de284daed6db5fd1186b37d0eabbb
abhijit <kamathabhijit2002@gmail.com>
2025-03-26 20:35:52 -0400

    got the program to share a simple html page

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,53 +1,74 @@
 package controllers
 
 import (
-	"context"
+	// "context"
 	"fmt"
-	"log"
+	// "log"
 	"net/http"
-	"time"
-
-	"github.com/coder/websocket"
-	"github.com/coder/websocket/wsjson"
+	"os"
+	// "time"
+	// "github.com/coder/websocket"
+	// "github.com/coder/websocket/wsjson"
 )
 
 var Router = http.NewServeMux()
 
 func goConvoHomePage(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		fmt.Println("getting the home page")
+	if r.Method != "GET" || r.URL.Path != "/" {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
 	}
+	fmt.Println("we are getting this far")
+	dir, _ := os.Getwd()
+	fmt.Println("current dir", dir)
+	http.ServeFile(w, r, "./templates/index.html")
 }
 
 func getChatHistory(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		fmt.Printf("getting the chat with id %v", r.PathValue("id"))
+	if r.Method != "GET" {
+		http.Error(w, "not found", http.StatusNotFound)
 	}
+
+	fmt.Printf("getting the chat with id %v", r.PathValue("id"))
 }
 
-func chatWebSocket(w http.ResponseWriter, r *http.Request) {
-	c, err := websocket.Accept(w, r, nil)
-	if err != nil {
-		log.Println("Error Accepting websocket connections", err)
-	}
-	defer c.CloseNow()
+// func chatWebSocket(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("we get here")
+// 	c, err := websocket.Accept(w, r, nil)
+// 	if err != nil {
+// 		log.Println("Error Accepting websocket connections", err)
+// 	}
+// 	defer c.CloseNow()
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
+// 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+// 	defer cancel()
 
-	var v interface{}
-	err = wsjson.Read(ctx, c, &v)
-	if err != nil {
-		log.Println("Error Reading from websocket", err)
-	}
+// 	for {
+// 		message := []byte("testing")
+// 		var currentIndex, length = 0, len(message)
+// 		written, err = c.Write(ctx,,message[currentIndex:length])
+// 		if err != nil {
+// 			log.Println("Error writing to websocket", err)
+// 		}
+// 		if currentIndex != length{
+// 			currentIndex += written
+// 			c.Write(message[currentIndex:length])
+// 		}
+// 		break
+// 	}
+// 	var v interface{}
+// 	err = wsjson.Read(ctx, c, &v)
+// 	if err != nil {
+// 		log.Println("Error Reading from websocket", err)
+// 	}
 
-	log.Printf("received: %v", v)
+// 	log.Printf("received: %v", v)
 
-	c.Close(websocket.StatusNormalClosure, "")
-}
+// 	c.Close(websocket.StatusNormalClosure, "")
+// }
 
 func CreateControllers() {
 	Router.HandleFunc("/", goConvoHomePage)
 	Router.HandleFunc("/chat/{id}", getChatHistory)
-	Router.HandleFunc("/chat/{id}/", chatWebSocket) // this will be on the route with "ws" prefix
+	// Router.HandleFunc("/chat/{id}/ws", chatWebSocket) // this will be on the route with "ws" prefix
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/abhijit360/GoConvo
 
 go 1.24.1
 
-require github.com/coder/websocket v1.8.13 // indirect
+require github.com/coder/websocket v1.8.13
