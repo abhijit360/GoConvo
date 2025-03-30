bcaa8b603b2ab8716fa48846bc87ff05db205f8d
abhijit <kamathabhijit2002@gmail.com>
2025-03-30 11:55:15 -0400

    adding a reader go routine, broadcaster go routine, go routine clean up

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,15 +1,13 @@
 package controllers
 
 import (
-	// "context"
+	"encoding/json"
 	"fmt"
-	"time"
-	// "log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/abhijit360/GoConvo/sessions"
-	// "time"
 	"github.com/gorilla/websocket"
 )
 
@@ -17,6 +15,10 @@ var Router = http.NewServeMux()
 var upgrader = websocket.Upgrader{
 	ReadBufferSize: 1024,
 	WriteBufferSize: 1024,
+}
+
+type createSessionRequest struct {
+	currentTime string `json:currentTime`
 }
 
 func goConvoHomePage(w http.ResponseWriter, r *http.Request) {
@@ -39,10 +41,27 @@ func getChatHistory(w http.ResponseWriter, r *http.Request) {
 }
 
 func createNewSession(w http.ResponseWriter, r * http.Request){
-	
+	if r.Method != "POST"{
+		http.Error(w,"Wrong HTTP method to create session",http.StatusBadRequest)
+	}
+	var response createSessionRequest
+	err := json.NewDecoder(r.Body).Decode(&response)
+	if err != nil {
+		fmt.Errorf("unable to parse json %v",err)
+		return
+	}
+	s, err := sessions.CreateSession(response.currentTime)
+
+	go s.HandleBroadcast() // create the broadcast
+
+	if err != nil {
+		fmt.Errorf("unable to create session %v",err)
+	}
+	chatMetaData := s.GetChatMetaData()
+	w.Write([]byte(chatMetaData.Chat_id))
 }
 
-func chatWebSocket(w http.ResponseWriter, r *http.Request) {
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	// create session or get existing session
 	chat_id := r.PathValue("id")
 	s, ok := sessions.GetSession(chat_id)
@@ -61,6 +80,28 @@ func chatWebSocket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	s.AddSession(conn)
+
+	defer conn.Close()
+	defer s.RemoveSession(conn)
+	done := make(chan struct{})
+
+	go func() {
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				close(done)
+				break
+			}
+			s.Broadcast <- message
+		}
+	}()
+
+	/* 
+	this maintains the connection until we want to explicitly break out.
+	if we do not have this we could end up with the race condition
+	where we end the connection before we even read from the connection 
+	due to the concurrent aspect of the go routine */
+	<- done
 }
 
 func CreateControllers() {
--- sessions/sessions-db-handler.go
+++ sessions/sessions-db-handler.go
@@ -1,7 +1,6 @@
 package sessions
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
@@ -17,18 +16,23 @@ const createNewSession = "INSERT INTO chats (expiry_date) VALUES (?)"
 const updateExistingSession = "UPDATE chats SET expiry_date = ? where chat_id == ?"
 
 type ChatMetaData struct {
-	chat_id string
-	expiry_date string
+	Chat_id     string
+	Expiry_date string
 }
 
-type Session struct{
-	subscribers map[*websocket.Conn]bool // array of subscriber websocket connections that we write to
-	broadcast chan []byte
-	chatMetaData ChatMetaData
-	db *sql.DB
+type Session struct {
+	Subscribers  map[*websocket.Conn]bool // array of subscriber websocket connections that we write to
+	Broadcast    chan []byte
+	ChatMetaData ChatMetaData
+	done         chan struct{}
+	DB           *sql.DB
 }
 
-type Mainnet struct{
+func (s *Session) HandleBroadcast() {
+	panic("unimplemented")
+}
+
+type Mainnet struct {
 	tracker map[string]*Session
 }
 
@@ -43,60 +47,61 @@ func init() {
 	}
 }
 
-func (s *Session)GetSession() (string,error){
-	if s.db == nil {
+func (s *Session) GetSession() (string, error) {
+	if s.DB == nil {
 		return "", fmt.Errorf("database not initialized")
 	}
 
 	sql_query := findExpiryBasedOnChatId
 
-    row := s.db.QueryRow(sql_query, s.chatMetaData.chat_id)
+	row := s.DB.QueryRow(sql_query, s.ChatMetaData.Chat_id)
 	var foundRow ChatMetaData
-	err := row.Scan(&foundRow.chat_id,&foundRow.expiry_date)
+	err := row.Scan(&foundRow.Chat_id, &foundRow.Expiry_date)
 	if err != nil {
-		if err == sql.ErrNoRows{
+		if err == sql.ErrNoRows {
 			return "", fmt.Errorf("chatId not found")
 		}
 		return "", err
 	}
 
-	return foundRow.expiry_date, nil
+	return foundRow.Expiry_date, nil
 }
 
-func CreateSession(current_time string) (*Session,error){
+func CreateSession(current_time string) (*Session, error) {
 	newSession := Session{
-		subscribers: make(map[*websocket.Conn]bool,0),
+		Subscribers: make(map[*websocket.Conn]bool, 0),
 	}
 	parsedTime, err := time.Parse(time.RFC3339, current_time)
 	if err != nil {
 		return &newSession, fmt.Errorf("Received time could not be parsed")
 	}
 	expiry_time := parsedTime.Add(24 * time.Hour) // fixed time addition
-	
+
 	if db == nil {
 		return nil, fmt.Errorf("database not initialized")
 	}
 
-	createdRow, err := db.Query(createNewSession,expiry_time)
+	createdRow, err := db.Query(createNewSession, expiry_time)
 	if err != nil {
 		return nil, err
 	}
-	
+
 	var chatMetaData ChatMetaData
-	err = createdRow.Scan(&chatMetaData.chat_id, &chatMetaData.expiry_date)
+	err = createdRow.Scan(&chatMetaData.Chat_id, &chatMetaData.Expiry_date)
 	if err != nil {
 		return nil, fmt.Errorf("error parsing row data into struct")
 	}
-	newSession.chatMetaData = chatMetaData
-	newSession.db = db
-	newSession.broadcast = make(chan []byte)
+	newSession.ChatMetaData = chatMetaData
+	newSession.DB = db
+	newSession.Broadcast = make(chan []byte)
+	newSession.done = make(chan struct{})
 
-	sessionManager.tracker[chatMetaData.chat_id] = &newSession
+	sessionManager.tracker[chatMetaData.Chat_id] = &newSession
 	return &newSession, nil
 }
 
-func (s *Session)UpdateSessionExpiryDate(current_time string) (string,error){
-	if s.db == nil {
+func (s *Session) UpdateSessionExpiryDate(current_time string) (string, error) {
+	if s.DB == nil {
 		return "", fmt.Errorf("database not initialized")
 	}
 	parsedTime, err := time.Parse(time.RFC3339, current_time)
@@ -105,47 +110,59 @@ func (s *Session)UpdateSessionExpiryDate(current_time string) (string,error){
 	}
 	expiry_time := parsedTime.Add(24 * time.Hour) // fixed time addition
 
-	updatedRow, err := s.db.Query(updateExistingSession,s.chatMetaData.chat_id,expiry_time)
-	
+	updatedRow, err := s.DB.Query(updateExistingSession, s.ChatMetaData.Chat_id, expiry_time)
+
 	if err != nil {
 		return "", err
 	}
-	
+
 	var chatMetaData ChatMetaData
 	updatedRow.Scan(&chatMetaData)
-	s.chatMetaData = chatMetaData
-	return chatMetaData.expiry_date, nil
+	s.ChatMetaData = chatMetaData
+	return chatMetaData.Expiry_date, nil
 }
 
-func (s *Session)RemoveSession(c *websocket.Conn){
-	delete(s.subscribers,c)
+func (s *Session) RemoveSession(c *websocket.Conn) {
+	delete(s.Subscribers, c)
+	if len(s.Subscribers) == 0 {
+		// reached a case where we have no more subscribers listening to this socket
+		close(s.done)
+		close(s.Broadcast)
+		delete(sessionManager.tracker, s.ChatMetaData.Chat_id)
+	}
 }
 
-func (s *Session)AddSession(c *websocket.Conn){
-	s.subscribers[c] = true
+func (s *Session) AddSession(c *websocket.Conn) {
+	s.Subscribers[c] = true
 }
 
-
-func GetSession(chat_id string) (*Session, bool){
+func GetSession(chat_id string) (*Session, bool) {
 	foundSession := Session{}
 	if session, ok := sessionManager.tracker[chat_id]; !ok {
 		return &foundSession, ok
-	}else {
+	} else {
 		return session, ok
 	}
 }
 
-func (s *Session) handleBroadcast(){
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	for message := range s.broadcast {
-		for conn, active := range s.subscribers{
-			if !active{
-				continue
+func (s *Session) handleBroadcast() {
+	for {
+		select {
+		case message := <-s.Broadcast:
+			for conn, active := range s.Subscribers {
+				if !active {
+					continue
+				}
+				if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
+					s.RemoveSession(conn)
+				}
 			}
-			if err := conn.Write(ctx,websocket.MessageText,message); err != nil {
-				s.RemoveSession(conn)
-			}
+		case <-s.done:
+			return
 		}
 	}
+}
+
+func (s *Session) GetChatMetaData() ChatMetaData {
+	return s.ChatMetaData
 }
