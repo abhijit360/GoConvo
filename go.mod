eeeaa62d519d5fb020abadb9e23162e3487fc761
abhijit <kamathabhijit2002@gmail.com>
2025-04-14 21:44:59 -0400

    fixing up the socket and writing the frontend using html templates

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/abhijit360/GoConvo
 
 go 1.24.1
 
-require github.com/mattn/go-sqlite3 v1.14.24
-
 require github.com/gorilla/websocket v1.5.3
--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"github.com/abhijit360/GoConvo/controllers"
+	"log"
 	"net/http"
+	"sync"
+	"text/template"
+	"path/filepath"
 )
 
+
+type templateHandler struct {
+	once sync.Once 
+	filename string
+	templ *template.Template
+}
+
+// loads the source file, compiles the template and executes it
+func (t* templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	t.once.Do(func(){
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w,nil)
+
+}
+
 func main() {
-	fmt.Println("starting program")
-	controllers.CreateControllers()
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: controllers.Router,
+	chat := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat",chat)
+	go chat.run()
+
+	if err := http.ListenAndServe(":8080",nil); err!= nil {
+		log.Fatal("ListenAndServe:",err)
 	}
-	server.ListenAndServe()
 }
