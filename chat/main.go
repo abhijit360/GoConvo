cc2666fc37abd1bc428fdf4fc8d9c73c83b370c3
abhijit <kamathabhijit2002@gmail.com>
2025-04-16 23:07:39 -0400

    handling the google auth and storing it as a cookie

--- chat/main.go
+++ chat/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
-	"github.com/abhijit360/GoConvo/trace"
-)
 
+	"github.com/abhijit360/GoConvo/trace"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/gomniauth"
+
+	// "github.com/stretchr/gomniauth/providers/facebook"
+	// "github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+)
 type templateHandler struct {
 	once     sync.Once
 	filename string
@@ -26,6 +33,21 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("Error retrieving auth key or auth secret from env file. .env file is potentially missing")
+	}
+	// setup goOmniAuth
+	gomniauth.SetSecurityKey("my-auth-key")
+	gomniauth.WithProviders(
+		// facebook.New("key", "secret",
+		// 	"http://localhost:8080/auth/callback/facebook"),
+		// github.New("key", "secret",
+		// 	"http://localhost:8080/auth/callback/github"),
+		google.New(os.Getenv("GOOGLE_AUTH_KEY"), os.Getenv("GOOGLE_AUTH_SECRET"),
+			"http://localhost:8080/auth/callback/google"),
+	)
+
 	chat := newRoom()
 	chat.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", RequireAuth(&templateHandler{filename: "chat.html"}))
